01846d60563def08e7b8e4281973d17c929c8362
nev1lle <ireginev5@gmail.com>
2022-10-27 17:15:41 +0000

    unsigned int x introduced in 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,18 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
+	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n; /* n changed to positive after placing '-' */
+		x = -n; /* n changed to positive after placing '-' */
 	}
 	if (n / 10)
-		print_number(n / 10); /**
+		print_number(x / 10); /**
 				       * _putchar not used since n/10 might not
 				       * be a single character
 				       */
-	_putchar((n % 10) + '0');/* last digit is output using _putchar */
+	_putchar((x % 10) + '0');/* last digit is output using _putchar */
 }
