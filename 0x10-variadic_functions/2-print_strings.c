07c963a1819f0b8a8ab98759615df3a1560d32e5
nev1lle <ireginev5@gmail.com>
2024-04-07 14:04:56 +0000

    Files 2-main.c, 2-print_strings.c & c created

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings -  prints strings, followed by a new line.
+ * @separator: string to be printed between the strings(a pointer)
+ * @n: number of strings passed to the function
+ *
+ * Description: 1. If separator is NULL, don’t print it
+ *              2. If one of the string is NULL, print (nil) instead
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+	char *str;
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strings, char *);
+
+		if (!separator)
+			printf("%s", str);
+		if (!str)
+			printf("nil");
+		else if (i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
+	}
+
+	va_end(strings);
+
+	printf("\n");
+
+}
