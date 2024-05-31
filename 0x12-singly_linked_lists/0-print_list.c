b4e467ba07f9be09dee43e08cb9b108b606257b8
nev1lle <ireginev5@gmail.com>
2024-05-31 14:43:02 +0000

    0-print_list.c modified

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: a pointer to an unmodifiable list whose elements are to be printed
- *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
