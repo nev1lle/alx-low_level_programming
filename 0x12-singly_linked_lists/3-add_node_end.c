225d55b0763d4e92584d07c202237510c9a7e654
nev1lle <ireginev5@gmail.com>
2024-06-04 16:04:04 +0000

    3-add_node_end.c modified to comply with betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * add_node - adds a new node at the end of a list_t list.
+ * add_node_end - adds a new node at the end of a list_t list.
  * @head: a pointer to a pointer to a list_t object
  * @str: a pointer to a constant character which has to be duplicated.
  *
@@ -10,13 +10,13 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *new;
+	list_t *new;
 	list_t *ptr = *head;
 
-        new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 
-        if (new == NULL)
-                return (NULL);
+	if (new == NULL)
+		return (NULL);
 
 	new->str = strdup(str);
 	new->len = strlen(str);
