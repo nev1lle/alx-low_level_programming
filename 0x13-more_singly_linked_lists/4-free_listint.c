d7eaa0c070ca2766cab023d61b3294ce9f9099ed
nev1lle <ireginev5@gmail.com>
2024-07-08 14:54:47 +0000

    Whitespace removed in file 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint - frees a listint_t list.
- * @head: a pointer of type listint_t to the start of a linked list. 
+ * @head: a pointer of type listint_t to the start of a linked list.
  */
 void free_listint(listint_t *head)
 {
