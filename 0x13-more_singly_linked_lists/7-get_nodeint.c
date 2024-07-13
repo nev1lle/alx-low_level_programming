aae801ab33bc1235e1fe35658a593b7a9cfeeb6f
nev1lle <ireginev5@gmail.com>
2024-07-13 16:35:09 +0000

    File 7-get_nodeint.c modified

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  * @index: index of the node, starting at 0
  *
  * Return: 1. nth node of a listint_t linked list.
-           2. NULL, if node does not exist
+ *           2. NULL, if node does not exist
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
